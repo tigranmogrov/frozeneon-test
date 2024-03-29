61820e80cd7313eb6fb7c3835c75d56192fb8c5b
Tigran Mogrov <tigranmogrov@gmail.com>
2024-03-29 13:40:08 +0200

    feat: create App and add components for View

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AppFooter from '@/components/AppFooter.vue';
+import AppHeader from '@/components/AppHeader.vue';
+import HomeView from '@/views/HomeView.vue';
+</script>
+
+<template>
+  <app-header />
+  <main :class="$style['main']">
+    <home-view />
+  </main>
+  <app-footer />
+</template>
+
+<style module lang="scss">
+.main {
+  display: block;
+  flex-grow: 1;
+  width: 100%;
+  max-width: 100%;
+  min-height: 1px;
+  padding-top: 40px;
+  padding-bottom: 40px;
+}
+</style>
