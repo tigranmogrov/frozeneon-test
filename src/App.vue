12a76839483142393cbcbdbb07f22d3e13158c36
Tigran Mogrov <tigranmogrov@gmail.com>
2024-03-29 16:26:52 +0200

    feat(App):  add fetch data if url has a query in start page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 import AppFooter from '@/components/AppFooter.vue';
 import AppHeader from '@/components/AppHeader.vue';
+import { usePackageStore } from '@/stores/package';
 import HomeView from '@/views/HomeView.vue';
+
+const packageStore = usePackageStore();
+const { fetchPackages } = packageStore;
+
+const currentParams = new URLSearchParams(window.location.search);
+const textQuery: string = currentParams.get('text') || '';
+const pageQuery: number = parseInt(currentParams.get('page') || '1');
+
+if (textQuery !== '') {
+  fetchPackages(textQuery, pageQuery);
+}
 </script>
 
 <template>
